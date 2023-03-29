1f0fbacf69c7f0503e14b26c352218cb8405fa0e
Ilya Zhigaras <zhigaras.dev@gmail.com>
2023-03-29 22:20:55 +0500

    letters field added

--- app/src/main/java/com/zhigaras/fiveletters/MainActivity.kt
+++ app/src/main/java/com/zhigaras/fiveletters/MainActivity.kt
@@ -25,10 +25,78 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-//                    Letter(letter = 'A')
+                    Letter(letter = 'A')
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Letter(
+    modifier: Modifier = Modifier,
+    letter: Char
+) {
+    OutlinedCard(
+        onClick = { /*TODO*/ },
+        border = BorderStroke(width = 2.dp, color = yellow),
+        colors = CardDefaults.cardColors(containerColor = gray),
+        modifier = modifier
+            .width(70.dp)
+            .padding(horizontal = 2.dp)
+    ) {
+        Text(
+            text = letter.toString().uppercase(),
+            style = MaterialTheme.typography.displayLarge,
+            modifier = modifier
+                .padding(8.dp)
+                .align(Alignment.CenterHorizontally)
+        )
+    }
+}
+
+@Composable
+fun LetterField(
+    modifier: Modifier = Modifier,
+    letters: List<List<Char>>
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(4.dp),
+        modifier = modifier
+            .fillMaxSize()
+            .padding(8.dp)
+    ) {
+        letters.forEach { letterRow ->
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                modifier = modifier.fillMaxWidth()
+            ) {
+                letterRow.forEach {
+                    Letter(letter = it)
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LetterPreview() {
+    FiveLettersTheme {
+        Letter(letter = 'a')
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun LetterFieldPreview() {
+    val row = listOf('a', 'b', 'c', 'ы', 'w')
+    val field = emptyList<List<Char>>().toMutableList()
+    repeat(6) {
+        field.add(row)
+    }
+    FiveLettersTheme {
+        LetterField(letters = field)
+    }
+}
