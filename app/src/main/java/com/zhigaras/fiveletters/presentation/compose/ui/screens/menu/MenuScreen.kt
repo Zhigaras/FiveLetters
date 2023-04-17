ae98173a8b602c2e441022a7f2d72649f1dc9ffc
Ilya Zhigaras <zhigaras.dev@gmail.com>
2023-04-17 19:13:03 +0500

    double back press exit implemented

--- app/src/main/java/com/zhigaras/fiveletters/presentation/compose/ui/screens/menu/MenuScreen.kt
+++ app/src/main/java/com/zhigaras/fiveletters/presentation/compose/ui/screens/menu/MenuScreen.kt
@@ -1,14 +1,17 @@
 package com.zhigaras.fiveletters.presentation.compose.ui.screens.menu
 
+import android.widget.Toast
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.zhigaras.fiveletters.MainActivity
 import com.zhigaras.fiveletters.R
 import com.zhigaras.fiveletters.model.UserStat
 import com.zhigaras.fiveletters.presentation.compose.ui.viewmodels.MenuViewModel
@@ -21,7 +24,20 @@ fun MenuScreen(
     
     // TODO: animated header
     
+    val context = LocalContext.current
+    val activity = context as? MainActivity
     val userStat by viewModel.userStatFlow().collectAsStateWithLifecycle(UserStat(0, 0f, 0, 0f))
+    var backPressedTime by remember { mutableStateOf(0L) }
+    BackHandler(enabled = true) {
+        if (backPressedTime + 2000 > System.currentTimeMillis()) activity?.finish()
+        else Toast.makeText(
+            context,
+            context.getString(R.string.press_back_again),
+            Toast.LENGTH_SHORT
+        ).show()
+        backPressedTime = System.currentTimeMillis()
+    }
+    
     Column(modifier = Modifier.fillMaxSize()) {
         Box(
             modifier = Modifier
