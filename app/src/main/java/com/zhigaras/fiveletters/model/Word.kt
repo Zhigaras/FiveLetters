195355a8bccab0f40bed49a98521c5e2a2b17318
Ilya Zhigaras <zhigaras.dev@gmail.com>
2023-05-07 03:23:50 +0500

    description field removed from word dataBase

--- app/src/main/java/com/zhigaras/fiveletters/model/Word.kt
+++ app/src/main/java/com/zhigaras/fiveletters/model/Word.kt
@@ -12,10 +12,9 @@ data class Word(
     val word: String,
     val solvedByUser: Boolean,
     val attempts: Int,
-    val description: String
 ) {
     companion object {
-        val mock = Word(1, "start", false, 0, "")
-        val rulesMock = Word(1, "песня", false, 0, "")
+        val mock = Word(1, "start", false, 0)
+        val rulesMock = Word(1, "песня", false, 0)
     }
 }
